d1d658cc38ce1a1af03cb3cedae3b5ed460c0414
luisaoli <luisaoliveira.e@gmail.com>
2022-04-14 12:56:22 -0300

    inclusao do schema e rotas para autor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import router from "./livrosRoutes.js";
+import livros from "./livrosRoutes.js";
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
@@ -8,7 +9,8 @@ const routes = (app) => {
 
     app.use(
         express.json(),
-        router
+        livros,
+        autores
     )
 }
 
