bfe876987b7f4524c5d0b985e04d1f2a8e18bcc6
luisaoli <luisaoliveira.e@gmail.com>
2022-04-14 11:52:32 -0300

    criacao dos controllers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from "./models/Livro.js"
+import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
@@ -11,21 +12,25 @@ const app = express();
 
 app.use(express.json())
 
+routes(app)
+
+//nao precisa mais da const porque os livros vem do banco de dados
 // const livros = [
 //     {id: 1, "titulo": "Senhor dos aneis"},
 //     {id: 2, "titulo": "O Hobbit"}
 // ]
 
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
+// rota foi para o index.js
+// app.get('/', (req, res) => {
+//     res.status(200).send('Curso de Node');
+// })
 
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros);
-    })
-    
-})
+//transferido pro controller
+// app.get('/livros', (req, res) => {
+//     livros.find((err, livros) => {
+//         res.status(200).json(livros);
+//     }) 
+// })
 
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
