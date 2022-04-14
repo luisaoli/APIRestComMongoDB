d51a512329d08173e97ad30a9b2b45cd265884a8
luisaoli <luisaoliveira.e@gmail.com>
2022-04-14 15:44:43 -0300

    inclusao de consulta diferenciada - busca por editora

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -61,6 +61,14 @@ class LivroController {
         })
     }
 
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
+        })
+    }
+
 }
 
 export default LivroController;
